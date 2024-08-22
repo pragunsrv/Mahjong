1dc87a1e92980dc16780e2eb05edfd9d03aadefe
Pragun <pragun1106@gmail.com>
2024-08-22 17:40:14 +0530

    Advanced
    added a scoring system, Players now accumulate points based on the number of melds, kongs, and flower tiles they have. The game will announce the points for each player at the end of the game and declare the player with the highest score as the winner. it also tracks winning tiles for additional gameplay complexity

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ class Mahjong:
         self.melds = [[] for _ in range(4)]
         self.kongs = [[] for _ in range(4)]
         self.flowers = [[] for _ in range(4)]
+        self.points = [0, 0, 0, 0]
+        self.winning_tiles = []
 
     def generate_tiles(self):
         tiles = []
@@ -96,6 +98,32 @@ class Mahjong:
             self.kongs[self.players.index(player)].append(tiles)
             for tile in tiles:
                 player.remove(tile)
+    def degenerate_tiles(self):
+        tiles = []
+        for suit in Mahjong.suits[:3]:
+            for rank in Mahjong.ranks:
+                for _ in range(4):
+                    tiles.append(Tile(suit, rank))
+        for wind in Mahjong.wind_tiles:
+            for _ in range(4):
+                tiles.append(Tile('Wind', wind))
+        for dragon in Mahjong.dragon_tiles:
+            for _ in range(4):
+                tiles.append(Tile('Dragon', dragon))
+        for i in range(1, 5):
+            self.flower_tiles.append(Tile('Flower', str(i)))
+            self.season_tiles.append(Tile('Season', str(i)))
+        tiles.extend(self.flower_tiles * 4)
+        tiles.extend(self.season_tiles * 4)
+        random.shuffle(tiles)
+        return tiles
+    def calculate_points(self, player):
+        points = 0
+        points += len(self.melds[player]) * 2
+        points += len(self.kongs[player]) * 8
+        points += len(self.flowers[player]) * 4
+        self.points[player] = points
+        return points
 
     def play_turn(self):
         drawn_tile = self.draw_from_wall()
@@ -104,6 +132,8 @@ class Mahjong:
             self.check_special_tiles(self.players[self.current_player], drawn_tile)
             self.discard_tile(self.players[self.current_player][0])
             win = self.check_for_win()
+            if win:
+                self.winning_tiles.append(drawn_tile)
             self.current_player = (self.current_player + 1) % 4
             self.turn_count += 1
             return win
@@ -126,6 +156,10 @@ class Mahjong:
             if self.turn_count >= 100:
                 print("Game ends in a draw due to too many turns.")
                 break
+        for i in range(4):
+            print(f"Player {i + 1} scored {self.calculate_points(i)} points.")
+        winning_player = self.points.index(max(self.points))
+        print(f"Player {winning_player + 1} wins the game with {self.points[winning_player]} points!")
 
 game = Mahjong()
 game.play_game()
